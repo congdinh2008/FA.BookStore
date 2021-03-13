c99a9c3bbaebad6fa2821b3c2179ba9f80644d7d
Cong Dinh <congdinhvannguyen165@outlook.com>
2021-03-14 00:38:06 +0700

    Create class and interface for generic repository with basic CRUD

---
+++ FA.BookStore.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FA.BookStore.Core.Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        TEntity Find(Guid id);
+
+        int Add(TEntity entity);
+
+        bool Update(TEntity entity);
+
+        bool Delete(TEntity entity);
+
+        IEnumerable<TEntity> GetAll();
+    }
+}
