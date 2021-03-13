518e26b867e047fb0dfd61bfea28d5f744da2b03
Cong Dinh <congdinhvannguyen165@outlook.com>
2021-03-14 00:39:56 +0700

    Apply generic repository for category

--- FA.BookStore.Core/Repositories/CategoryRepository.cs
+++ FA.BookStore.Core/Repositories/CategoryRepository.cs
@@ -1,45 +1,9 @@
-﻿using System;
-using FA.BookStore.Core.Data;
-using FA.BookStore.Core.Models;
-using System.Collections.Generic;
-using System.Data.Entity.Migrations;
-using System.Linq;
+﻿using FA.BookStore.Core.Models;
 
 namespace FA.BookStore.Core.Repositories
 {
-    public class CategoryRepository : ICategoryRepository
+    public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
     {
-        private readonly BookStoreContext _context;
-        public CategoryRepository()
-        {
-            _context = new BookStoreContext();
-        }
-        public Category Find(Guid categoryId)
-        {
-            return _context.Categories.Find(categoryId);
-        }
 
-        public int Add(Category category)
-        {
-            _context.Categories.Add(category);
-            return _context.SaveChanges();
-        }
-
-        public bool Update(Category category)
-        {
-            _context.Categories.AddOrUpdate(category);
-            return _context.SaveChanges() > 0;
-        }
-
-        public bool Delete(Category category)
-        {
-            _context.Categories.Remove(category);
-            return _context.SaveChanges() > 0;
-        }
-
-        public IEnumerable<Category> GetAll()
-        {
-            return _context.Categories.ToList();
-        }
     }
 }
